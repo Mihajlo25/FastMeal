76451ecd55baa94f2ad6412e595d90f3e18489fe
Mihajlo <58790004+Mihajlo25@users.noreply.github.com>
2023-02-10 10:31:21 +0100

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,7 @@
+        <footer class="footer text-center py-1">
+            <div>
+            <a>&copy; <?php echo date('Y') ?> FastMeal Team </a>           
+            </div>
+        </footer>
+    </body>
+</html>
