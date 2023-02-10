74d1356095f7cd1196259e256d6e3c2a70c07218
Mihajlo <58790004+Mihajlo25@users.noreply.github.com>
2023-02-10 10:21:05 +0100

    Create body.php

---
+++ baza/includes/body.php
@@ -0,0 +1,40 @@
+
+<link rel="stylesheet" href="<?= DIR_CSS . 'style.css'?>">
+
+
+<section class="container-fluid px-0 ">
+      <div class="row align-items-center mx-0">
+        <div id="relativan" class="col-md-6 order-2 order-md-1  ">
+          <h3 class="opisStranice">Opis stranice klikni ovde <!--<a href="isplanirajObrok.html">Isplaniraj obrok</a><--></h3>
+        </div>
+        <div  class="col-md-6 order-1 order-md-2 px-0 mx-0">
+          <img src="<?= DIR_IMG . 'hranaa.jpg' ?>" class="img-fluid">
+        </div>
+      </div>
+    </section>
+
+    <section class="container px-0">
+      <div class="container">
+        <div class="row align-items-center text-center">
+          <div class="col-md-6 order-2 order-md-1 px-0">
+            <img src="<?= DIR_IMG . 'hranaa.jpg' ?>" class="img-fluid">
+          </div>
+          <div class="textBox align-items-center col-md-6 order-1 order-md-2">
+            <h4>Recept</h4>
+            <p class="px-5 p">Still court no small think death so an wrote. Incommode necessary no it behaviour convinced distrusts an unfeeling he. Could death since do we hoped is in. Exquisite no my attention extensive. The determine conveying moonlight age. Avoid for see marry sorry child. Sitting so totally forbade hundred to. </p>          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="container px-0">
+      <div class="container">
+        <div class="row align-items-center text-center">
+          <div class="col-md-6 order-2 order-md-2 px-0">
+            <img src="<?= DIR_IMG . 'hranaa.jpg' ?>" class="img-fluid">
+          </div>
+          <div class="textBox align-items-center col-md-6 order-1 order-md-1">
+            <h4>Recept</h4>
+            <p>Still court no small think death so an wrote. Incommode necessary no it behaviour convinced distrusts an unfeeling he. Could death since do we hoped is in. Exquisite no my attention extensive. The determine conveying moonlight age. Avoid for see marry sorry child. Sitting so totally forbade hundred to. </p>          </div>
+        </div>
+      </div>
+    </section>
